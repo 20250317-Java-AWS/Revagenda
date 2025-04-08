dca283fd9e6fc629cc3759b2682332c213833ccd
LiquidPlummer <kyle.plummer@revature.net>
2025-04-08 14:06:53 -0400

    Added an example derived query, and an example custom query

--- revagenda-backend/src/main/java/com/revature/revagenda_backend/repositories/UserRepository.java
+++ revagenda-backend/src/main/java/com/revature/revagenda_backend/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.revature.revagenda_backend.repositories;
 
 import com.revature.revagenda_backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,5 +15,12 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     getAll()
     delete(Integer id)
      */
+
+    //Derived query methods
+    User findUserByUsername(String username);
+
+    //Custom Queries - either native SQL or HQL
+    @Query(value = "FROM users WHERE username = 'Kyle'", nativeQuery = false)
+    User findKyle();
 
 }
